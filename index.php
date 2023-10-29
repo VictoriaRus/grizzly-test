0103a10e29c6286e5bc5a5457c3bd0b318d4a8df
Вика <ruseckaya@cmit.ru>
2023-10-29 11:19:32 +0300

    add function for popup

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["send-cookies"])) {
+    setcookie("cookie", "cookie-connected", time() + 86400);
+}
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -14,7 +20,15 @@
             right: 0;
             bottom: 0;
             background-color: #DDDDDD;
+            transform: translateY(100%);
+            transition: transform .2s ease;
         }
+
+        .is-show {
+            transform: translateY(0);
+            transition: transform .2s ease;
+        }
+
         @media screen and (max-width: 578px) {
             .modal-popup {
                 position: relative;
@@ -61,12 +75,31 @@
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    const cookiePopup = document.querySelector(".modal-popup--js");
+    const btnClose = document.querySelector(".btn-close--js");
+    const btnOk = document.querySelector(".btn-ok--js");
+
+    if (!document.cookie) {
+        cookiePopup.classList.add("is-show");
+    }
+
+    btnClose.addEventListener("click", () => {
+        cookiePopup.classList.remove("is-show");
+    });
+
+    btnOk.addEventListener("click", () => {
+        cookiePopup.classList.remove("is-show");
+    });
+
+</script>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossorigin="anonymous"></script>
 </body>
 </html>
-
 
 <?php
 /**
