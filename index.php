fcd7a820e49f0c4e0bf02b691e0eb5222a6f3ff6
Вика <ruseckaya@cmit.ru>
2023-10-29 11:34:45 +0300

    update layout

--- index.php
+++ index.php
@@ -71,6 +71,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["send-cookies"])) {
                     padding: 32px 24px;
                 }
             }
+
         </style>
     </head>
     <body>
@@ -97,7 +98,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["send-cookies"])) {
             </div>
         </div>
     </div>
-
     <section class="advantages pt-5 pb-5">
         <div class="container d-lg-none">
             <h2 class="mb-4">Korzyści ze współpracy z nami</h2>
@@ -385,7 +385,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["send-cookies"])) {
             </div>
         </div>
     </section>
-
     <div class="modal-popup modal-popup--js p-4">
         <div class="modal-popup__content row">
             <div class="modal-popup__body col-xxl-10 mb-3">
@@ -400,7 +399,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["send-cookies"])) {
             </div>
         </div>
     </div>
-
     <script type="text/javascript">
         const cookiePopup = document.querySelector(".modal-popup--js");
         const btnClose = document.querySelector(".btn-close--js");
@@ -419,7 +417,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["send-cookies"])) {
         });
 
     </script>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
             crossorigin="anonymous"></script>
